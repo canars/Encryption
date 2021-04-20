75c5f2f9f6639fdbc0517f301c2d02611b213122
xcg3747 <xcg3747@CP13993.inhouse.ad.in.gad.de>
2021-04-20 16:12:17 +0200

    Encryption is ready.

--- Encryption/src/EncryptionProgram.java
+++ Encryption/src/EncryptionProgram.java
@@ -6,11 +6,10 @@ public class EncryptionProgram
     private Scanner scanner;
     private Random random;
     private ArrayList<Character> list;
-    private ArrayList<Character> shufeldList;
+    private ArrayList<Character> shuffeldList;
     private char character;
     private String line;
     private char[] letters;
-    private char[] secretLetters;
 
 
     EncryptionProgram()
@@ -18,7 +17,7 @@ public class EncryptionProgram
         scanner = new Scanner(System.in);
         random = new Random();
         list = new ArrayList();
-        shufeldList = new ArrayList();
+        shuffeldList = new ArrayList();
         character = ' ';
 
         newKey();
@@ -32,33 +31,106 @@ public class EncryptionProgram
             System.out.println("***************************************************");
             System.out.println("What do you want to do?");
             System.out.println("(N)ew Key, (G)et Key, (E)ncrypt, (D)ecrypt, (Q)uit");
-            
+
             char response = Character.toUpperCase(scanner.nextLine().charAt(0));
+
+            switch (response)
+            {
+                case 'N' :
+                    newKey();
+                    break;
+                case 'G' :
+                    getKey();
+                    break;
+                case 'E' :
+                    encrypt();
+                    break;
+                case 'D' :
+                    decrypt();
+                    break;
+                case 'Q' :
+                    quit();
+                    break;
+                default :
+                    System.out.println("Not a valid input..");
+            }
         }
     }
 
     private void newKey()
     {
-
+        character = ' ';
+        list.clear();
+        shuffeldList.clear();
+        
+        for(int i = 32; i<127; i++) {
+            list.add(Character.valueOf(character));
+            character++;
+        }
+        shuffeldList = new ArrayList(list);
+        Collections.shuffle(shuffeldList);
+        System.out.println("** A new key has been generated.");
     }
 
     private void getKey()
     {
-
-    }
+        System.out.println("Key: " );
+        for(Character x : list) {
+            System.out.print(x);
+        }
+        System.out.println();
+        for(Character x : shuffeldList) {
+            System.out.print(x);
+        }
+        System.out.println();
+        }
 
     private void encrypt()
     {
-
+        System.out.println("Enter a message to be encrypted..");
+        String message = scanner.nextLine();
+        
+        letters = message.toCharArray();
+        
+        for (int i = 0; i<letters.length;i++) {
+            for(int j =0; j<list.size(); j++) {
+                if(letters[i] == list.get(j)) {
+                letters[i] = shuffeldList.get(j);
+                break;
+            }
+        }
     }
-
+        System.out.println("Encrypted: ");
+        for(char x : letters) {
+            System.out.print(x);
+        }
+        System.out.println();
+    }
     private void decrypt()
     {
-
+        System.out.println("Enter a message to be dencrypted..");
+        String message = scanner.nextLine();
+        
+        letters = message.toCharArray();
+        
+        for (int i = 0; i<letters.length;i++) {
+            for(int j =0; j<shuffeldList.size(); j++) {
+                if(letters[i] == shuffeldList.get(j)) {
+                letters[i] = list.get(j);
+                break;
+            }
+        }
+    }
+        System.out.println("Decrypted: ");
+        for(char x : letters) {
+            System.out.print(x);
+        }
+        System.out.println();
     }
 
     private void quit()
     {
-
+        System.out.println("thanks");
+        System.exit(0);
     }
 }
